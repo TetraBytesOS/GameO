54798daf1d39ef90a11500fb17cf8959bb3199ab
TetraBytesOS <grupofso2025@gmail.com>
2025-03-05 00:07:43 +0100

    Add files via upload

---
+++ Game/Minigames/Tamagochi/UsoTamagochi.java
@@ -0,0 +1,13 @@
+package univerisdadProgramacion;
+
+public class UsoTamagochi {
+
+	public static void main(String[] args) {
+
+        Tamagochi ElBicho=new Tamagochi();
+        
+    
+        
+	}
+
+}
