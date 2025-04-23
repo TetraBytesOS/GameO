63ef62c5e7ecb8d33c5fccfbc423639207c8238e
TetraBytesOS <grupofso2025@gmail.com>
2025-04-23 09:39:37 +0200

    Delete Game/Minigames directory

--- Game/Minigames/BlowFish(Maze)/PezGloboOcean.java
+++
@@ -1,8 +0,0 @@
-public class PezGloboOcean {/*Solo  es el main para ejecutar el programa. */
-    public static void main (String[] args){
-    PezGlobo pez = new PezGlobo("TetraByte",1,"PezGlobo"); /*muestra la informacion del pez globo: Nombre: TetraByte, Especie: Pez Globo, Edad: 1 año  */
-    pez.mostrarInfo();
-        Juego juego=new Juego ();/*Creo la instancia del jugo */
-        juego.iniciar();/* Inicio juego */
-    }
-}
